538de48fae6bd67e20f42dbe2fd74f646a154fff
Kauã Araujo de Lima <112039006+LKaua22k@users.noreply.github.com>
2023-07-15 13:17:51 -0300

    :tada:

--- index.js
+++ index.js
@@ -3,3 +3,16 @@ const pause = document.querySelector(".pause")
 const stop = document.querySelector(".stop")
 const more = document.querySelector(".more")
 const less = document.querySelector(".less")
+let minutes = document.querySelector(".minutes")
+let timeout
+
+function coutdown() {
+    setTimeout(() => {
+        
+    }, 1000);
+}
+
+play.addEventListener("click", () =>{
+    alert("play")
+
+})
